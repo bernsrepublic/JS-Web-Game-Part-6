6e2e3c9edc9ade752418daf963e87904006b39b1
bernsrepublic <bernsrepublic@gmail.com>
2023-03-10 19:22:13 -0800

    added async/await function

--- index.js
+++ index.js
@@ -5,19 +5,41 @@ const pc = newPlayableCharacter(100, 110);
 const npc = newNonPlayableCharacter(50, 300);
 
 // have the NPC start walking east immediately
-npc.walkNorth(1400, () => {
-  npc.walkEast(1200, () => {
-    npc.walkSouth(300, () => {
-      npc.walkEast(1500, () => {
-        npc.walkSouth(1500, () => {
-          npc.walkWest(2700, () => {
-            npc.walkNorth(400);
-          });
-        });
-      });
-    });
-  });
-});
+// npc.walkNorth(1400, () => {
+//   npc.walkEast(1200, () => {
+//     npc.walkSouth(300, () => {
+//       npc.walkEast(1500, () => {
+//         npc.walkSouth(1500, () => {
+//           npc.walkWest(2700, () => {
+//             npc.walkNorth(400);
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
+// previous code block was replaced by npc to clean up the code ^
+// npc
+//   .walkNorth(1400)
+//   .then(() => npc.walkEast(1200))
+//   .then(() => npc.walkSouth(300))
+//   .then(() => npc.walkEast(1500))
+//   .then(() => npc.walkSouth(1500))
+//   .then(() => npc.walkWest(2700))
+//   .then(() => npc.walkNorth(400));
+
+// .then code block replaced by async/await (below)
+
+async function moveNPC() {
+  await npc.walkNorth(1400);
+  await npc.walkEast(1200);
+  await npc.walkSouth(300);
+  await npc.walkEast(1500);
+  await npc.walkSouth(1500);
+  await npc.walkWest(2700);
+  await npc.walkNorth(400);
+}
+moveNPC();
 
 // Create the inventory
 const inventory = newInventory();
